d294d697313def223bcd6c701f61eac5897ed6e0
Plaqueminier <antoinebazin34@gmail.com>
2020-02-22 13:08:30 +0100

    Add skill menu and upgrade of skills

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -8,25 +8,35 @@ public class MenuScript : MonoBehaviour
     public List<Button> buttons = new List<Button>();
     int index;
     Button selectedButton;
+
+    bool verticalAxisKey;
     void Start()
     {
         index = 0;
         selectedButton = null;
+        verticalAxisKey = false;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown("Down") && index < buttons.Count - 1)
+        if (Input.GetAxisRaw("Vertical") == -1 && index < buttons.Count - 1 && !verticalAxisKey)
         {
             index += 1;
             selectedButton = buttons[index];
             UpdateSelectedButton();
-        } else if (Input.GetKeyDown("Up") && index > 0)
+            verticalAxisKey = true;
+        }
+        else if (Input.GetAxisRaw("Vertical") == 1 && index > 0 && !verticalAxisKey)
         {
             index -= 1;
             selectedButton = buttons[index];
             UpdateSelectedButton();
-        } else if (Input.GetButtonDown("Submit"))
+            verticalAxisKey = true;
+        } else if (Input.GetAxisRaw("Vertical") == 0 && verticalAxisKey)
+        {
+            verticalAxisKey = false;
+        }
+        if (Input.GetButtonDown("Submit"))
         {
             selectedButton.onClick.Invoke();
         }
