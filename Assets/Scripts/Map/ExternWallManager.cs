8d62890e26c50a16734b3a7beb4aad285610aa5c
Fournier <thomas.fournier@epitech.eu>
2020-02-22 22:16:03 +0100

    modif externwall

---
+++ Assets/Scripts/Map/ExternWallManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExternWallManager : MonoBehaviour
+{
+    public Transform t1;
+    public Transform t2;
+    public Transform t3;
+    public Transform t4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 camPos = Camera.main.ScreenToWorldPoint(new Vector3(0,Screen.height, 0));
+        t1.position = camPos;
+        t4.position = camPos;
+        camPos = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width,0, 0));
+        t2.position = camPos;
+        t3.position = camPos;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
