8ebe8c379d2c56fca1dab3db63ab5bfce7c80802
Plaqueminier <antoinebazin34@gmail.com>
2020-02-22 19:44:53 +0100

    Player Damageable

--- Assets/Scripts/DamageableObj.cs
+++ Assets/Scripts/DamageableObj.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DamagableObj : MonoBehaviour
+public class DamageableObj : MonoBehaviour
 {
     // Start is called before the first frame update
     public int hp;
@@ -12,17 +12,11 @@ public class DamagableObj : MonoBehaviour
     virtual public void takeDamage(int damage)
     {
         hp -= damage;
-        Debug.Log(damage);
+        Debug.Log(name + " has taken " + damage + " damage, it now has " + hp + " health points !");
         if (hp <= 0) {
             hp = 0;
-            Debug.Log("death");
+            Debug.Log(name + " is dead.");
             Destroy(gameObject);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
