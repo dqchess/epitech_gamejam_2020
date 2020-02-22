ac63fbbccecb59651f5d867bc2611d02f020107c
Plaqueminier <antoinebazin34@gmail.com>
2020-02-22 16:37:42 +0100

    add attacker stats

---
+++ Assets/Scripts/Player/AttackerStats.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerStats : MonoBehaviour
+{
+
+    #region Singleton
+
+    public static AttackerStats instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("There is another AttackerStats instance");
+            return;
+        }
+        instance = this;
+    }
+
+    #endregion
+
+    public int crystals = 0;
+    public int health = 100;
+    public int damage = 100;
+}
