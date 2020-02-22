34efd8e4cbd2a65c909ee5cd7a29dccad16c583f
hanglove34 <nicolas.nguyen@epitech.eu>
2020-02-22 13:17:38 +0100

    change touch

--- Assets/2D Space Kit/Scripts/ExampleShipControl.cs
+++ Assets/2D Space Kit/Scripts/ExampleShipControl.cs
@@ -21,44 +21,35 @@ public class ExampleShipControl : MonoBehaviour {
 	
 	
 	
-		if (Input.GetKey(KeyCode.W)) {
+		if (Input.GetKey(KeyCode.Z)) {//forward
 			GetComponent<Rigidbody2D>().AddForce(transform.up * acceleration_amount * Time.deltaTime);
 		
 		}
-		if (Input.GetKey(KeyCode.S)) {
-			GetComponent<Rigidbody2D>().AddForce((-transform.up) * acceleration_amount * Time.deltaTime);
+		// if (Input.GetKey(KeyCode.S)) {//back
+		// 	GetComponent<Rigidbody2D>().AddForce((-transform.up) * acceleration_amount * Time.deltaTime);
 			
-		}
+		// }
 		
-		if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.LeftShift)) {
+		if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.Q)) {//slide left
 			GetComponent<Rigidbody2D>().AddForce((-transform.right) * acceleration_amount * 0.6f  * Time.deltaTime);
 			//print ("strafeing");
 		}
-		if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.LeftShift)) {
+		if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D)) {//slide right
 			GetComponent<Rigidbody2D>().AddForce((transform.right) * acceleration_amount * 0.6f  * Time.deltaTime);
 			
 		}
 		
-		if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.LeftShift)) {
+		if (!Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D)) {//turn right
 			GetComponent<Rigidbody2D>().AddTorque(-rotation_speed  * Time.deltaTime);
 			
 		}
-		if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.LeftShift)) {
+		if (!Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.Q)) {//turn left
 			GetComponent<Rigidbody2D>().AddTorque(rotation_speed  * Time.deltaTime);
 			
 		}	
-		if (Input.GetKey(KeyCode.C)) {
+		if (Input.GetKey(KeyCode.S)) {//stop move
 			GetComponent<Rigidbody2D>().angularVelocity = Mathf.Lerp(GetComponent<Rigidbody2D>().angularVelocity, 0, rotation_speed * 0.06f * Time.deltaTime);
 			GetComponent<Rigidbody2D>().velocity = Vector2.Lerp(GetComponent<Rigidbody2D>().velocity, Vector2.zero, acceleration_amount * 0.06f * Time.deltaTime);
 		}	
-		
-		
-		if (Input.GetKey(KeyCode.H)) {
-			transform.position = new Vector3(0,0,0);
-		}	
-		
-		
-		
-		
 	}
 }
