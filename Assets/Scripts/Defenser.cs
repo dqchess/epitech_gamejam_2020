35584e477895e55ea98217fd9d7828900a94001a
Plaqueminier <antoinebazin34@gmail.com>
2020-02-23 04:00:40 +0100

    oui

---
+++ Assets/Scripts/Defenser.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Defenser : MonoBehaviour
+{
+
+    #region Singleton
+
+    public static Defenser instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("ATTENTION !!!!");
+            return;
+        }
+        instance = this;
+    }
+
+    #endregion
+
+    public float crystals;
+    public float gain;
+    float t;
+
+    void Start()
+    {
+        t = 0;
+    }
+
+    void Update()
+    {
+        t += Time.deltaTime;
+        if (t > 1f) {
+            crystals += gain; 
+            t = 0;
+        }
+    }
+}
